d2546c10aac5bbabcfbe7e17d40b8f9c402366f3
Eshat <eshatjubayer22@outlook.com>
2025-09-10 20:12:20 +0600

    done

---
+++ app/views.py
@@ -0,0 +1,23 @@
+# todo/views.py
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+
+    return render(request, "app/index.html", {"tasks": tasks, "form": form})
+
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect("/")
+
